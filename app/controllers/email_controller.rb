627753d6459c3b06440b8d02f9cfd8c0a77ad4fe
hs-seo <pcnshs@gmail.com>
2016-03-20 00:36:39 +0900

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,19 @@ class EmailController < ApplicationController
 	end
 
 	def email_send
-		@email = params[:email]
-		@content = params[:content]
+		email = params[:email]
+		title = params[:title]
+		content = params[:content]
+
+		gmail = Gmail.connect("pcnshs@gmail.com", "tjgudtjr1!")
+		gmail.deliver do
+			to email
+			subject tilte
+			text_part do
+				body content
+			end
+		end
+
+		gmail.logout
 	end
 end
